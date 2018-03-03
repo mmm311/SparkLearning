2769e7b096a5a2826601d9b20b34fe37f9933c0c
mmm311 <1034930026@qq.com>
2018-03-03 20:58:44 +0800

    注释 ValidateBinarySearchTree.java

--- src/main/scala/com/leetcode/tree/ValidateBinarySearchTree.java
+++ src/main/scala/com/leetcode/tree/ValidateBinarySearchTree.java
@@ -1,26 +1,26 @@
-package com.leetcode.tree;
-class TreeNode{
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int x){
-        val = x;
-    }
-}
-public class ValidateBinarySearchTree {
-    boolean validate = true;
-    public boolean isValidBST(TreeNode root) {
-        return false;
-    }
-
-    public boolean isValidBSTRecursive(TreeNode root){
-        if (root == null){
-            return true;
-        }
-
-        boolean left = root.left != null ? isValidBSTRecursive(root.left): true;
-        boolean right = root.right != null ? isValidBSTRecursive(root.right): true;
-
-
-    }
-}
+//package com.leetcode.tree;
+//class TreeNode{
+//    int val;
+//    TreeNode left;
+//    TreeNode right;
+//    TreeNode(int x){
+//        val = x;
+//    }
+//}
+//public class ValidateBinarySearchTree {
+//    boolean validate = true;
+//    public boolean isValidBST(TreeNode root) {
+//        return false;
+//    }
+//
+//    public boolean isValidBSTRecursive(TreeNode root){
+//        if (root == null){
+//            return true;
+//        }
+//
+//        boolean left = root.left != null ? isValidBSTRecursive(root.left): true;
+//        boolean right = root.right != null ? isValidBSTRecursive(root.right): true;
+//
+//
+//    }
+//}
