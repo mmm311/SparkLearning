2d574ae06b3775697008a6fe409e6af11aed9aec
mmm311 <1034930026@qq.com>
2018-03-03 21:07:09 +0800

    307. Range Sum Query - Mutable

---
+++ src/main/scala/com/leetcode/tree/NumArray.java
@@ -0,0 +1,16 @@
+package com.leetcode.tree;
+
+public class NumArray {
+    public NumArray(int[] nums){
+
+    }
+
+    public void update(int i, int val){
+
+    }
+
+    public int sumRange(int i, int j){
+
+    }
+
+}
