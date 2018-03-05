f929a803d8c6435685aef8351554f36a789625b1
mmm311 <1034930026@qq.com>
2018-03-05 19:42:41 +0800

    自己写的，哈哈哈

--- src/main/scala/com/leetcode/tree/NumArray.java
+++ src/main/scala/com/leetcode/tree/NumArray.java
@@ -4,60 +4,57 @@ package com.leetcode.tree;
  * @author liu
  */
 public class NumArray {
-    private SegmentTreeNode root = null;
+    private SegmentTree root;
 
     public NumArray(int[] nums){
-        root = buildTree(nums, 0, nums.length - 1);
+        root = buildSegmentTree(nums, 0, nums.length - 1);
     }
 
     public void update(int i, int val){
         update(root, i, val);
-
     }
 
     public int sumRange(int i, int j){
         return sumRange(root, i, j);
     }
 
-    /**
-     * 建树
-     * @param nums 数组
-     * @param start 开始位置
-     * @param end 结束位置
-     * @return SegmentTreeNode 根节点
-     */
-    private SegmentTreeNode buildTree(int[] nums, int start, int end){
-        if (start > end){
-            return null;
-        }else{
-            SegmentTreeNode ret = new SegmentTreeNode(start, end);
-            if (start == end){
-                ret.sum = nums[start];
-            }else{
-                int mid = start + (end - start) / 2;
-                ret.left = buildTree(nums, start, mid);
-                ret.right = buildTree(nums, mid + 1, end);
-                ret.sum = ret.left.sum + ret.right.sum;
-            }
-            return ret;
+    class SegmentTree{
+        int start, end;
+        int sum;
+        SegmentTree  left, right;
+        public SegmentTree(int start, int end){
+            this.start = start;
+            this.end = end;
+            this.left = null;
+            this.right = null;
+            this.sum = -1;
         }
     }
 
+    private SegmentTree buildSegmentTree(int[] nums, int start, int end){
+        if (start > end){
+            return null;
+        }else{
+            SegmentTree node = new SegmentTree(start, end);
+            if (start == end){
+                node.sum = nums[start];
+            }else{
+                int mid = (start + end) / 2;
+                node.left = buildSegmentTree(nums, start, mid);
+                node.right = buildSegmentTree(nums, mid + 1, end);
+                node.sum = node.left.sum + node.right.sum;
+            }
+            return node;
+        }
+    }
 
-    /**
-     * 单点修改
-     * @param root 根节点
-     * @param pos 位置
-     * @param val 修改值
-     */
-
-    private void update(SegmentTreeNode root, int pos, int val){
+    private void update(SegmentTree root, int pos, int val){
         if (root.start == root.end){
             root.sum = val;
         }else{
-            int mid = root.start + (root.end - root.start) / 2;
-            if (pos <= mid){
-                update(root.left, pos ,val);
+            int mid = (root.start + root.end) / 2;
+            if (pos <= mid) {
+                update(root.left,  pos, val);
             }else{
                 update(root.right, pos, val);
             }
@@ -65,40 +62,19 @@ public class NumArray {
         }
     }
 
-    /**
-     * 区间求和
-     * @param root 根结点
-     * @param start 开始位置
-     * @param end 结束位置
-     * @return 区间和
-     */
-    private int sumRange(SegmentTreeNode root, int start, int end){
-        if (root.end == end && root.start == start){
-            return root.sum;
-        }else{
-            int mid = root.start + (root.end - root.start) / 2;
-            if (end <= mid){
-                return sumRange(root.left, start, end);
-            }else if (start >= mid + 1){
-                return sumRange(root.right, start, end);
-            }else{
-                return sumRange(root.right, mid + 1, end) +
-                        sumRange(root.left, start, mid);
-            }
-        }
-    }
-    class SegmentTreeNode{
-        int start, end;
-        SegmentTreeNode left, right;
-        int sum;
-
-        public SegmentTreeNode(int start, int end){
-            this.start = start;
-            this.end = end;
-            this.left = null;
-            this.right = null;
-            this.sum = 0;
-        }
+    private int sumRange(SegmentTree root, int start, int end){
+       if (root.start == start && root.end == end){
+           return root.sum;
+       }else{
+           int mid = (root.start + root.end) / 2;
+           if (end <= mid){
+              return sumRange(root.left, start, end);
+           }else if( mid + 1 <= start){
+               return sumRange(root.right, start, end);
+           }else {
+               return sumRange(root.left, start, mid) + sumRange(root.right, mid + 1, end);
+           }
+       }
     }
 
 }
