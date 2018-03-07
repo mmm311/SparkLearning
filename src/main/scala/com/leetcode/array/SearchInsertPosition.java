1e4aba01c2d9c563777fd5eeb708c0d3d08cbdf3
mmm311 <1034930026@qq.com>
2018-03-07 16:30:16 +0800

    Search Insert Position

---
+++ src/main/scala/com/leetcode/array/SearchInsertPosition.java
@@ -0,0 +1,22 @@
+package com.leetcode.array;
+
+/**
+ * @author
+ * 利用二分法找出插入位置
+ */
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target){
+        int low = 0, high = nums.length - 1;
+        while (low <= high){
+            int mid = (low + high) / 2;
+            if (nums[mid] == target){
+                return mid;
+            }else if (nums[mid] > target){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return low;
+     }
+}
