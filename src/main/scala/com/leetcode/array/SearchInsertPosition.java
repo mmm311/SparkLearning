f1202efbc24f93772685cd867116b56435d36c4c
mmm311 <1034930026@qq.com>
2018-03-07 17:01:58 +0800

    Search Insert Position

--- src/main/scala/com/leetcode/array/SearchInsertPosition.java
+++ src/main/scala/com/leetcode/array/SearchInsertPosition.java
@@ -8,7 +8,7 @@ public class SearchInsertPosition {
     public int searchInsert(int[] nums, int target){
         int low = 0, high = nums.length - 1;
         while (low <= high){
-            int mid = (low + high) / 2;
+            int mid =  low + (high - low) / 2;
             if (nums[mid] == target){
                 return mid;
             }else if (nums[mid] > target){
